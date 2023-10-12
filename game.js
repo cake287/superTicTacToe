77a6c821d457ae2858ed2d22078fd6846fcdd612
cake287 <edcake0123@gmail.com>
2023-10-12 13:57:06 +0100

    resizable

--- game.js
+++ game.js
@@ -20,7 +20,7 @@ function main() {
         boxes[i].major.classList.add("majorBox");
         gameContainer.appendChild(boxes[i].major);
 
-        boxes[i].major.style.borderWidth = borderExists[i].map((exists) => (0.5*exists) + "em").join(" ");
+        boxes[i].major.style.borderWidth = borderExists[i].map((exists) => (0.35*exists) + "em").join(" ");
         
 
         for (let j = 0; j < 9; j++) {
@@ -32,8 +32,26 @@ function main() {
         } 
     }
 
+    //winResize();
+}
+
+function winResize() {
+    let winSize = Math.min(window.innerWidth, window.innerHeight);
+    //console.log(winSize);
+    for (let i = 0; i < document.styleSheets.length; i++)
+        for (let rule of document.styleSheets[i].cssRules) {
+            if (rule.selectorText == ".majorBox") {
+                rule.style["width"] = winSize * 0.18 + "px";
+                rule.style["height"] = winSize * 0.18 + "px";
+                rule.style["padding"] = winSize * 0.03 + "px";
+            } 
+            else if (rule.selectorText == ".minorBox") {
+                rule.style["width"] = winSize * 0.06 + "px";
+                rule.style["height"] = winSize * 0.06 + "px";
+            }
+        }
 }
 
 
-
+window.onresize = winResize;
 window.onload = main;

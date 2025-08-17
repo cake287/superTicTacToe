7f55050dcaaab6d4efe9a47ea6dcf6c370feb3b5
Eddie Smith <edcake0123@gmail.com>
2025-08-17 14:38:28 +0100

    Revert "more ai"
    This reverts commit eb6716e632cb8595648a49c6f6440bfe468fc8dd.

--- game.js
+++ game.js
@@ -6,7 +6,6 @@ let isTurnCrosses = true;
 const crossChar = "\u2573";
 const circleChar = "\u25EF";
 
-
 // board is a list of boxes (major or minor) 
 // returns ["X", lineStartIndex, lineEndIndex], ["O", lineStartIndex, lineEndIndex] or [" "]
 function testGameWin(board) {
@@ -64,6 +63,7 @@ function boxClick(majorIndex, minorIndex) {
             line.dataset.startIndex = lineStartIndex;
             line.dataset.endIndex = lineEndIndex;
             thisMajor.appendChild(line);
+            console.log(line);
 
             reformatLine(line);
         }
@@ -89,8 +89,19 @@ function boxClick(majorIndex, minorIndex) {
         gc.add(isTurnCrosses ? "crossTurn" : "circleTurn");
 
         if (singleplayer) {
-            board = getCurrentBoard();
-            //printBoard(board);
+            let intBoard = [];
+            for (let maj = 0; maj < 9; maj++) {
+                intBoard[maj] = []
+                for (let min = 0; min < 9; min++) {
+                    let val = 0;
+                    if (boxes[maj].minors[min].dataset.value == "X")
+                        val = 1;
+                    else if (boxes[maj].minors[min].dataset.value == "O")
+                        val = -1;
+                    intBoard[maj][min] = val;
+                }
+            }
+            printBoard(intBoard);
         }
     }
 }
